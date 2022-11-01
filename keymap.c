6ac8887b6da2086cdc390fb670fc6990182664f4
Dakota <marshalldakota549@gmail.com>
2022-11-01 18:58:09 -0400

    add numpad layout

--- keymap.c
+++ keymap.c
@@ -21,7 +21,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_F19, KC_F20, KC_F21, KC_TRNS,
                      KC_MPRV, KC_MPLY, KC_MNXT),
 
-        [1] = LAYOUT(TO(0), KC_NO, KC_TRNS, KC_TRNS,
+        [1] = LAYOUT(TO(0), KC_NUM, TO(2), KC_TRNS,
+                     KC_P7, KC_P8, KC_P9, KC_MUTE,
+                     KC_P4, KC_P5, KC_P6, KC_TRNS,
+                     KC_P1, KC_P2, KC_P3),
+        
+        [2] = LAYOUT(TO(1), KC_NO, KC_TRNS, KC_TRNS,
                      KC_NO, KC_NO, KC_VOLU, KC_MUTE,
                      KC_NO, KC_NO, KC_NO, KC_TRNS,
                      KC_NO, KC_NO, KC_VOLD),
